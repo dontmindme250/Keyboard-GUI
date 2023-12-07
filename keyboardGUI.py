dea9ac0948819fb45e882f753af8cb98080720aa
V7q <149380921+Vumacc@users.noreply.github.com>
2023-12-07 20:10:06 +0500

    Update keyboardGUI.py
    Made the UI easier on the eyes

--- keyboardGUI.py
+++ keyboardGUI.py
@@ -3,18 +3,13 @@ import ttkbootstrap as ttk
 import tkinter as tk
 
 # System Settings
-ctk.set_appearance_mode('System')
+ctk.set_appearance_mode('light')
 ctk.set_default_color_theme('blue')
 
 # App Frame
 root = ctk.CTk()
 root.geometry('500x300')
 root.title('Virtual keyboard')
-
-label = tk.Label()
-textbox = tk.Text()
-button = tk.Button()
-
 
 def btn1_clicked():
     print('1', end='')
@@ -131,127 +126,132 @@ def btn_z_clicked():
 
 buttonframe = ctk.CTkFrame(root)
 buttonframe.columnconfigure(0, weight=1)
-letterframe = ttk.Frame(root)
+
+letterframe = ctk.CTkFrame(root)
 letterframe.columnconfigure(0, weight=3)
 
-btn1 = tk.Button(buttonframe, padx=10, text='1', font=('Roboto', 24, 'bold'), command=btn1_clicked)
-btn1.grid(row=0, column=0, sticky='news')
+spacebarframe = ctk.CTkFrame(root)
+spacebarframe.columnconfigure(0, weight=4)
 
-btn2 = tk.Button(buttonframe, padx=10, text='2', font=('Roboto', 24, 'bold'), command=btn2_clicked)
-btn2.grid(row=0, column=1, sticky='news')
+btn1 = ctk.CTkButton(buttonframe, width=9, height=7, text='1', font=('Consolas', 28, 'bold'), command=btn1_clicked)
+btn1.grid(row=0, column=0, sticky='news', padx=2)
 
-btn3 = tk.Button(buttonframe, padx=10, text='3', font=('Roboto', 24, 'bold'), command=btn3_clicked)
-btn3.grid(row=0, column=2, sticky='news')
+btn2 = ctk.CTkButton(buttonframe, width=9, height=7, text='2', font=('Consolas', 28, 'bold'), command=btn2_clicked)
+btn2.grid(row=0, column=1, sticky='news', padx=2)
 
-btn4 = tk.Button(buttonframe, padx=10, text='4', font=('Roboto', 24, 'bold'), command=btn4_clicked)
-btn4.grid(row=0, column=3, sticky='news')
+btn3 = ctk.CTkButton(buttonframe, width=9, height=7, text='3', font=('Consolas', 28, 'bold'), command=btn3_clicked)
+btn3.grid(row=0, column=2, sticky='news', padx=2)
 
-btn5 = tk.Button(buttonframe, padx=10, text='5', font=('Roboto', 24, 'bold'), command=btn5_clicked)
-btn5.grid(row=0, column=4, sticky='news')
+btn4 = ctk.CTkButton(buttonframe, width=9, height=7, text='4', font=('Consolas', 28, 'bold'), command=btn4_clicked)
+btn4.grid(row=0, column=3, sticky='news', padx=2)
 
-btn6 = tk.Button(buttonframe, padx=10, text='6', font=('Roboto', 24, 'bold'), command=btn6_clicked)
-btn6.grid(row=0, column=5, sticky='news')
+btn5 = ctk.CTkButton(buttonframe, width=9, height=7, text='5', font=('Consolas', 28, 'bold'), command=btn5_clicked)
+btn5.grid(row=0, column=4, sticky='news', padx=2)
 
-btn7 = tk.Button(buttonframe, padx=10, text='7', font=('Roboto', 24, 'bold'), command=btn7_clicked)
-btn7.grid(row=0, column=6, sticky='news')
+btn6 = ctk.CTkButton(buttonframe, width=9, height=7, text='6', font=('Consolas', 28, 'bold'), command=btn6_clicked)
+btn6.grid(row=0, column=5, sticky='news', padx=2)
 
-btn8 = tk.Button(buttonframe, padx=10, text='8', font=('Roboto', 24, 'bold'), command=btn8_clicked)
-btn8.grid(row=0, column=7, sticky='news')
+btn7 = ctk.CTkButton(buttonframe, width=9, height=7, text='7', font=('Consolas', 28, 'bold'), command=btn7_clicked)
+btn7.grid(row=0, column=6, sticky='news', padx=2)
 
-btn9 = tk.Button(buttonframe, padx=10, text='9', font=('Roboto', 24, 'bold'), command=btn9_clicked)
-btn9.grid(row=0, column=8, sticky='news')
+btn8 = ctk.CTkButton(buttonframe, width=9, height=7, text='8', font=('Consolas', 28, 'bold'), command=btn8_clicked)
+btn8.grid(row=0, column=7, sticky='news', padx=2)
 
-btn0 = tk.Button(buttonframe, padx=10, text='0', font=('Roboto', 24, 'bold'), command=btn0_clicked)
-btn0.grid(row=0, column=9, sticky='news')
+btn9 = ctk.CTkButton(buttonframe, width=9, height=7, text='9', font=('Consolas', 28, 'bold'), command=btn9_clicked)
+btn9.grid(row=0, column=8, sticky='news', padx=2)
 
-buttonframe.pack(padx=20, pady=20)
+btn0 = ctk.CTkButton(buttonframe, width=9, height=7, text='0', font=('Consolas', 28, 'bold'), command=btn0_clicked)
+btn0.grid(row=0, column=9, sticky='news', padx=2)
 
+buttonframe.pack(pady=20)
 
-btn_a = tk.Button(letterframe, text='A', font=('Roboto', 22, 'bold'), command=btn_a_clicked)
-btn_a.grid(row=0, column=0)
+# SPECIAL CHARACTERS GO HERE
+# WILL BE ADDED LATER
+# (TRUST)
 
-btn_b = tk.Button(letterframe, text='B', font=('Roboto', 22, 'bold'), command=btn_b_clicked)
+btn_a = ctk.CTkButton(letterframe, width=7, height=7, text='A', font=('Consolas', 28, 'bold'), command=btn_a_clicked)
+btn_a.grid(row=0, column=0, padx=2, pady=2)
+
+btn_b = ctk.CTkButton(letterframe, width=7, height=7, text='B', font=('Consolas', 28, 'bold'), command=btn_b_clicked)
 btn_b.grid(row=0, column=1)
 
-btn_c = tk.Button(letterframe, text='C', font=('Roboto', 22, 'bold'), command=btn_c_clicked)
+btn_c = ctk.CTkButton(letterframe, width=7, height=7, text='C', font=('Consolas', 28, 'bold'), command=btn_c_clicked)
 btn_c.grid(row=0, column=2)
 
-btn_d = tk.Button(letterframe, text='D', font=('Roboto', 22, 'bold'), command=btn_d_clicked)
+btn_d = ctk.CTkButton(letterframe, width=7, height=7, text='D', font=('Consolas', 28, 'bold'), command=btn_d_clicked)
 btn_d.grid(row=0, column=3)
 
-btn_e = tk.Button(letterframe, text='E', font=('Roboto', 22, 'bold'), command=btn_e_clicked)
+btn_e = ctk.CTkButton(letterframe, width=7, height=7, text='E', font=('Consolas', 28, 'bold'), command=btn_e_clicked)
 btn_e.grid(row=0, column=4)
 
-btn_f = tk.Button(letterframe, text='F', font=('Roboto', 22, 'bold'), command=btn_f_clicked)
+btn_f = ctk.CTkButton(letterframe, width=7, height=7, text='F', font=('Consolas', 28, 'bold'), command=btn_f_clicked)
 btn_f.grid(row=0, column=5)
 
-btn_g = tk.Button(letterframe, text='G', font=('Roboto', 22, 'bold'), command=btn_g_clicked)
+btn_g = ctk.CTkButton(letterframe, width=7, height=7, text='G', font=('Consolas', 28, 'bold'), command=btn_g_clicked)
 btn_g.grid(row=0, column=6)
 
-btn_h = tk.Button(letterframe, text='H', font=('Roboto', 22, 'bold'), command=btn_h_clicked)
+btn_h = ctk.CTkButton(letterframe, width=7, height=7, text='H', font=('Consolas', 28, 'bold'), command=btn_h_clicked)
 btn_h.grid(row=0, column=7)
 
-btn_i = tk.Button(letterframe, text='I', font=('Roboto', 22, 'bold'), command=btn_i_clicked)
+btn_i = ctk.CTkButton(letterframe, width=7, height=7, text='I', font=('Consolas', 28, 'bold'), command=btn_i_clicked)
 btn_i.grid(row=0, column=8)
 
-btn_j = tk.Button(letterframe, text='J', font=('Roboto', 22, 'bold'), command=btn_j_clicked)
+btn_j = ctk.CTkButton(letterframe, width=7, height=7, text='J', font=('Consolas', 28, 'bold'), command=btn_j_clicked)
 btn_j.grid(row=0, column=9)
 
-btn_k = tk.Button(letterframe, text='K', font=('Roboto', 22, 'bold'), command=btn_k_clicked)
+btn_k = ctk.CTkButton(letterframe, width=7, height=7, text='K', font=('Consolas', 28, 'bold'), command=btn_k_clicked)
 btn_k.grid(row=0, column=10)
 
-btn_l = tk.Button(letterframe, text='L', font=('Roboto', 22, 'bold'), command=btn_l_clicked)
+btn_l = ctk.CTkButton(letterframe, width=7, height=7, text='L', font=('Consolas', 28, 'bold'), command=btn_l_clicked)
 btn_l.grid(row=0, column=11)
 
-btn_m = tk.Button(letterframe, text='M', font=('Roboto', 22, 'bold'), command=btn_m_clicked)
+btn_m = ctk.CTkButton(letterframe, width=7, height=7, text='M', font=('Consolas', 28, 'bold'), command=btn_m_clicked)
 btn_m.grid(row=0, column=12)
 
-btn_n = tk.Button(letterframe, text='N', font=('Roboto', 22, 'bold'), command=btn_n_clicked)
+btn_n = ctk.CTkButton(letterframe, width=7, height=7, text='N', font=('Consolas', 28, 'bold'), command=btn_n_clicked)
 btn_n.grid(row=0, column=13)
 
-btn_o = tk.Button(letterframe, text='O', font=('Roboto', 22, 'bold'), command=btn_o_clicked)
+btn_o = ctk.CTkButton(letterframe, width=7, height=7, text='O', font=('Consolas', 28, 'bold'), command=btn_o_clicked)
 btn_o.grid(row=1, column=1)
 
-btn_p = tk.Button(letterframe, text='P', font=('Roboto', 22, 'bold'), command=btn_p_clicked)
+btn_p = ctk.CTkButton(letterframe, width=7, height=7, text='P', font=('Consolas', 28, 'bold'), command=btn_p_clicked)
 btn_p.grid(row=1, column=2)
 
-btn_q = tk.Button(letterframe, text='Q', font=('Roboto', 22, 'bold'), command=btn_q_clicked)
+btn_q = ctk.CTkButton(letterframe, width=7, height=7, text='Q', font=('Consolas', 28, 'bold'), command=btn_q_clicked)
 btn_q.grid(row=1, column=3)
 
-btn_r = tk.Button(letterframe, text='R', font=('Roboto', 22, 'bold'), command=btn_r_clicked)
+btn_r = ctk.CTkButton(letterframe, width=7, height=7, text='R', font=('Consolas', 28, 'bold'), command=btn_r_clicked)
 btn_r.grid(row=1, column=4)
 
-btn_s = tk.Button(letterframe, text='S', font=('Roboto', 22, 'bold'), command=btn_s_clicked)
+btn_s = ctk.CTkButton(letterframe, width=7, height=7, text='S', font=('Consolas', 28, 'bold'), command=btn_s_clicked)
 btn_s.grid(row=1, column=5)
 
-btn_t = tk.Button(letterframe, text='T', font=('Roboto', 22, 'bold'), command=btn_t_clicked)
+btn_t = ctk.CTkButton(letterframe, width=7, height=7, text='T', font=('Consolas', 28, 'bold'), command=btn_t_clicked)
 btn_t.grid(row=1, column=6)
 
-btn_u = tk.Button(letterframe, text='U', font=('Roboto', 22, 'bold'), command=btn_u_clicked)
+btn_u = ctk.CTkButton(letterframe, width=7, height=7, text='U', font=('Consolas', 28, 'bold'), command=btn_u_clicked)
 btn_u.grid(row=1, column=7)
 
-btn_v = tk.Button(letterframe, text='V', font=('Roboto', 22, 'bold'), command=btn_v_clicked)
+btn_v = ctk.CTkButton(letterframe, width=7, height=7, text='V', font=('Consolas', 28, 'bold'), command=btn_v_clicked)
 btn_v.grid(row=1, column=8)
 
-btn_w = tk.Button(letterframe, text='W', font=('Roboto', 22, 'bold'), command=btn_w_clicked)
+btn_w = ctk.CTkButton(letterframe, width=7, height=7, text='W', font=('Consolas', 28, 'bold'), command=btn_w_clicked)
 btn_w.grid(row=1, column=9)
 
-btn_x = tk.Button(letterframe, text='X', font=('Roboto', 22, 'bold'), command=btn_x_clicked)
+btn_x = ctk.CTkButton(letterframe, width=7, height=7, text='X', font=('Consolas', 28, 'bold'), command=btn_x_clicked)
 btn_x.grid(row=1, column=10)
 
-btn_y = tk.Button(letterframe, text='Y', font=('Roboto', 22, 'bold'), command=btn_y_clicked)
+btn_y = ctk.CTkButton(letterframe, width=7, height=7, text='Y', font=('Consolas', 28, 'bold'), command=btn_y_clicked)
 btn_y.grid(row=1, column=11)
 
-btn_z = tk.Button(letterframe, text='Z', font=('Roboto', 22, 'bold'), command=btn_o_clicked)
+btn_z = ctk.CTkButton(letterframe, width=7, height=7, text='Z', font=('Consolas', 28, 'bold'), command=btn_o_clicked)
 btn_z.grid(row=1, column=12)
 
-btn_spacebar = tk.Button(letterframe, text='  ', font=('arial'), command=btn_spacebar_clicked)
-btn_spacebar.grid(row=1, column=0)
+letterframe.pack(ipady=5, ipadx=0, pady=2)
 
-btn_spacebar = tk.Button(letterframe, text='  ', font=('arial'), command=btn_spacebar_clicked)
-btn_spacebar.grid(row=1, column=13)
+btn_spacebar = ctk.CTkButton(spacebarframe, width=3, height=3, text='SPACEBAR', font=('Consolas', 28, 'bold'), command=btn_spacebar_clicked)
+btn_spacebar.grid(row=0, column=0)
 
-letterframe.pack()
-
+spacebarframe.pack(ipady=4, ipadx=168)
 
 root.mainloop()
